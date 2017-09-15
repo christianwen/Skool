1a91481deafafe0f08d3850d7a402359d292e4aa
Christian Gunney <tienanhnguyen0108@gmail.com>
2017-09-15 17:29:01 +0700

    fix sign in bug

--- app/src/main/java/com/example/tiena/amsconnection/activity/SignInActivity.java
+++ app/src/main/java/com/example/tiena/amsconnection/activity/SignInActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.Toast;
@@ -34,32 +35,7 @@ public class SignInActivity extends AppCompatActivity {
 
 
         if(auth.getCurrentUser()!=null){
-
-            FirebaseDatabase.getInstance().getReference("students/"+auth.getCurrentUser().getUid()+"/class_id")
-                    .addListenerForSingleValueEvent(new ValueEventListener() {
-                @Override
-                public void onDataChange(DataSnapshot dataSnapshot) {
-                    Toast.makeText(SignInActivity.this, dataSnapshot.getValue(String.class), Toast.LENGTH_SHORT).show();
-                    if(dataSnapshot.getValue()==null){
-                        startActivity(new Intent(SignInActivity.this,RequestInfoActivity.class));
-                    }
-                    else{
-                        //startActivity(new Intent(SignInActivity.this,RequestInfoActivity.class));
-
-                        Intent intent=new Intent(SignInActivity.this,MainActivity.class);
-                        intent.putExtra("class_id",dataSnapshot.getValue(String.class));
-                        startActivity(intent);
-                    }
-                }
-
-                @Override
-                public void onCancelled(DatabaseError databaseError) {
-
-                }
-            });
-
-
-
+            checkClass();
         }
         else{
             startActivityForResult(
@@ -88,12 +64,10 @@ public class SignInActivity extends AppCompatActivity {
             if (resultCode == RESULT_OK) {
 
                 IdpResponse idpResponse = IdpResponse.fromResultIntent(data);
-                Intent intent=new Intent(this,RequestInfoActivity.class);
-                /*startActivity(new Intent(this, MainActivity.class)
-                        .putExtra("my_token", idpResponse.getIdpToken()));
-*/
+                checkClass();
+
                 finish();
-                return;
+
             } else {
                 // Sign in failed
 
@@ -112,7 +86,7 @@ public class SignInActivity extends AppCompatActivity {
 
                 if (response.getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
                     showSnackbar(getResources().getString(R.string.unknown_error));
-                    return;
+
                 }
             }
 
@@ -121,7 +95,7 @@ public class SignInActivity extends AppCompatActivity {
     }
 
     private void showSnackbar(String message){
-        Snackbar.make(parentLayout, "This is main activity", Snackbar.LENGTH_LONG)
+        Snackbar.make(parentLayout,message, Snackbar.LENGTH_LONG)
                 .setAction("CLOSE", new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
@@ -130,5 +104,34 @@ public class SignInActivity extends AppCompatActivity {
                 })
                 .setActionTextColor(getResources().getColor(android.R.color.holo_red_light ))
                 .show();
+    }
+
+    private void checkClass(){
+        FirebaseDatabase.getInstance().getReference("students/"+FirebaseAuth.getInstance().getCurrentUser().getUid()+"/class_id")
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        Toast.makeText(SignInActivity.this, dataSnapshot.getValue(String.class), Toast.LENGTH_SHORT).show();
+                        if(dataSnapshot.getValue()==null){
+                            Toast.makeText(SignInActivity.this, "didnt have class id", Toast.LENGTH_SHORT).show();
+
+                            Log.d("class_id","none");
+                            startActivity(new Intent(SignInActivity.this,RequestInfoActivity.class));
+                        }
+                        else{
+                            //startActivity(new Intent(SignInActivity.this,RequestInfoActivity.class));
+                            Toast.makeText(SignInActivity.this, "already had class id", Toast.LENGTH_SHORT).show();
+                            Log.d("class_id","already has");
+                            Intent intent=new Intent(SignInActivity.this,MainActivity.class);
+                            intent.putExtra("class_id",dataSnapshot.getValue(String.class));
+                            startActivity(intent);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
     }
 }
